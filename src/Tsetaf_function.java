9295cbad269b7ab80c1d5bb42212d4ac1c1f3e6b
zhujinlong <jinlong1206@outlook.com>
2020-08-04 17:36:07 +0800

    finish the terminal interface

--- src/Tsetaf_function.java
+++ src/Tsetaf_function.java
@@ -4,6 +4,7 @@ import org.graphstream.ui.view.Viewer;
 import java.io.File;
 import java.io.IOException;
 import java.text.Format;
+import java.util.Scanner;
 
 public class Tsetaf_function {
     public static void main(String[] args) {
@@ -11,32 +12,58 @@ public class Tsetaf_function {
         Tsetaf tsetaf = parser.Parse("/Users/zhujinlong/Downloads/TSETAF/file/TSETAF1.apx");
         //  type:0();1(];2[];3[)
         Setaf setaf = tsetaf.atTime(new Availability_interval(110, 500, 1));
-    /*
-	 System.out.println("please select the function:");
-        System.out.println("1. generate tsetaf\n2. generate setaf\n3. generate tsetaf graph\n4. generate setaf graph");
-        Scanner scanner = new Scanner(System.in);
-        int next = scanner.nextInt();
-        if (next == 1) {
-            System.out.println(tsetaf);
-        } else if (next == 2) {
-            System.out.println(setaf);
-        } else if (next == 3) {
-            Graph G1 = tsetaf.getGraph();
-            write(G1);
-            Viewer vTSETAF = G1.display();
-        }else if(next==4){
-            Graph G2 = setaf.getGraph();
-            write(G2);
-            Viewer vSETAF = G2.display();
+        int next = -1;
+        while (next != 0) {
+            System.out.println("please select the function:");
+            System.out.println("0-----exit");
+            System.out.println("1-----generate tsetaf");
+            System.out.println("2-----generate setaf");
+            System.out.println("3-----generate tsetaf graph");
+            System.out.println("4-----generate setaf graph");
+            System.out.println("5-----show setaf HN-categoriser");
+            Scanner scanner = new Scanner(System.in);
+            next = scanner.nextInt();
+            switch (next) {
+                case 0:
+                    break;
+                case 1:
+                    System.out.println(tsetaf);
+                    break;
+                case 2:
+                    System.out.println(setaf);
+                    break;
+                case 3:
+                    Graph G1 = tsetaf.getGraph();
+                    write(G1);
+                    Viewer vTSETAF = G1.display();
+                    break;
+                case 4:
+                    Graph G2 = setaf.getGraph();
+                    write(G2);
+                    Viewer vSETAF = G2.display();
+                    break;
+                case 5:
+                    System.out.println(setaf.getHN_categoriser(10));
+                    break;
+                case 6:
+                    try {
+                        setaf.convertAspartix();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    System.out.println("please input the correct number:");
+                    break;
+            }
         }
-
-
+/*
         try {
             setaf.convertAspartix();
         } catch (IOException e) {
             e.printStackTrace();
         }
-	*/
+
         System.out.println(setaf);
         System.out.println(tsetaf);
 
@@ -53,7 +80,7 @@ public class Tsetaf_function {
         //   Viewer vTSETAF = G1.display();
         //Viewer vSETAF = G2.display();
 
-        System.out.println(setaf.getHN_categoriser(10));
+        System.out.println(setaf.getHN_categoriser(10));*/
 
     }
 
